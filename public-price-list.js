7d4fbe9e646cc815bb05dd5bbccc6748ed678398
DLeonProyects <soyalexdeleonproyect@gmail.com>
2025-10-24 17:01:13 -0400

    style: remove background color from modal overlay

--- public-price-list.js
+++ public-price-list.js
@@ -289,7 +289,6 @@
             left: 0;
             right: 0;
             bottom: 0;
-            background-color: rgba(0, 0, 0, 0.5);
             backdrop-filter: blur(10px);
             -webkit-backdrop-filter: blur(10px);
             display: flex;
