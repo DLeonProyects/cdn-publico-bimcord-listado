addcc2998a3e075ccf9bf45c1afecaf9ae3da4a8
DLeonProyects <soyalexdeleonproyect@gmail.com>
2025-10-27 16:55:52 -0400

    vfvfvfvf

--- public-price-list.js
+++ public-price-list.js
@@ -236,7 +236,7 @@
         tableCell: `
             padding: 1rem 1.5rem;
             font-size: clamp(1.5rem, 3.8vw, 1.2rem);
-            line-height: 1.25rem;
+            line-height: 3rem;
             color: #a8a29e;
             white-space: nowrap;
             text-align: center;
